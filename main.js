43dc87d02d43d72c46edf4317a971d6d06228031
Zain Latif <zainlatif702@gmail.com>
2025-09-28 19:03:14 +0500

    Remove Eye Animation

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var audio = document.getElementById("audioPlayer"),
   loader = document.getElementById("preloader");
+
 function settingtoggle() {
   document
     .getElementById("setting-container")
@@ -11,23 +12,28 @@ function settingtoggle() {
       .getElementById("soundtogglebuttoncontainer")
       .classList.toggle("soundmodeshow");
 }
+
 function playpause() {
   !1 == document.getElementById("switchforsound").checked
     ? audio.pause()
     : audio.play();
 }
+
 function visualmode() {
   document.body.classList.toggle("light-mode"),
     document.querySelectorAll(".needtobeinvert").forEach(function (e) {
       e.classList.toggle("invertapplied");
     });
 }
+
 window.addEventListener("load", function () {
   (loader.style.display = "none"),
-    document.querySelector(".hey").classList.add("popup");
+    document.querySelector(".Hi").classList.add("popup");
 });
+
 let emptyArea = document.getElementById("emptyarea"),
   mobileTogglemenu = document.getElementById("mobiletogglemenu");
+
 function hamburgerMenu() {
   document.body.classList.toggle("stopscrolling"),
     document
@@ -43,6 +49,7 @@ function hamburgerMenu() {
       .getElementById("burger-bar3")
       .classList.toggle("hamburger-animation3");
 }
+
 function hidemenubyli() {
   document.body.classList.toggle("stopscrolling"),
     document
@@ -58,11 +65,13 @@ function hidemenubyli() {
       .getElementById("burger-bar3")
       .classList.remove("hamburger-animation3");
 }
+
 const sections = document.querySelectorAll("section"),
   navLi = document.querySelectorAll(".navbar .navbar-tabs .navbar-tabs-ul li"),
   mobilenavLi = document.querySelectorAll(
     ".mobiletogglemenu .mobile-navbar-tabs-ul li"
   );
+
 window.addEventListener("scroll", () => {
   let e = "";
   sections.forEach((t) => {
@@ -77,57 +86,33 @@ window.addEventListener("scroll", () => {
       t.classList.remove("activeThistab"),
         t.classList.contains(e) && t.classList.add("activeThistab");
     });
-}),
-  console.log(
-    "%c Designed and Developed by Vinod Jangid ",
-    "background-image: linear-gradient(90deg,#8000ff,#6bc5f8); color: white;font-weight:900;font-size:1rem; padding:20px;"
-  );
+});
+
+console.log(
+  "%c Designed and Developed by Vinod Jangid ",
+  "background-image: linear-gradient(90deg,#8000ff,#6bc5f8); color: white;font-weight:900;font-size:1rem; padding:20px;"
+);
+
 let mybutton = document.getElementById("backtotopbutton");
+
 function scrollFunction() {
   document.body.scrollTop > 400 || document.documentElement.scrollTop > 400
     ? (mybutton.style.display = "block")
     : (mybutton.style.display = "none");
 }
+
 function scrolltoTopfunction() {
   (document.body.scrollTop = 0), (document.documentElement.scrollTop = 0);
 }
-(window.onscroll = function () {
+
+window.onscroll = function () {
   scrollFunction();
-}),
-  document.addEventListener(
-    "contextmenu",
-    function (e) {
-      "IMG" === e.target.nodeName && e.preventDefault();
-    },
-    !1
-  );
-let Pupils = document.getElementsByClassName("footer-pupil"),
-  pupilsArr = Array.from(Pupils),
-  pupilStartPoint = -10,
-  pupilRangeX = 20,
-  pupilRangeY = 15,
-  mouseXStartPoint = 0,
-  mouseXEndPoint = window.innerWidth,
-  currentXPosition = 0,
-  fracXValue = 0,
-  mouseYEndPoint = window.innerHeight,
-  currentYPosition = 0,
-  fracYValue = 0,
-  mouseXRange = mouseXEndPoint - mouseXStartPoint;
-const mouseMove = (e) => {
-    (fracXValue =
-      (currentXPosition = e.clientX - mouseXStartPoint) / mouseXRange),
-      (fracYValue = (currentYPosition = e.clientY) / mouseYEndPoint);
-    let t = pupilStartPoint + fracXValue * pupilRangeX,
-      o = pupilStartPoint + fracYValue * pupilRangeY;
-    pupilsArr.forEach((e) => {
-      e.style.transform = `translate(${t}px, ${o}px)`;
-    });
+};
+
+document.addEventListener(
+  "contextmenu",
+  function (e) {
+    "IMG" === e.target.nodeName && e.preventDefault();
   },
-  windowResize = (e) => {
-    (mouseXEndPoint = window.innerWidth),
-      (mouseYEndPoint = window.innerHeight),
-      (mouseXRange = mouseXEndPoint - mouseXStartPoint);
-  };
-window.addEventListener("mousemove", mouseMove),
-  window.addEventListener("resize", windowResize);
+  !1
+);
